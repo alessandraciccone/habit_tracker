3c956e3e19eccfdd8e91143e042628474905c1b3
alessandraciccone <alessandraciccone@icloud.com>
2026-01-07 16:56:35 +0100

    update database.py

--- app/database.py
+++ app/database.py
@@ -1,6 +1,22 @@
-from app.database import engine, Base
-from app.models_sql import *
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-print("Creating database tables...")
-Base.metadata.create_all(bind=engine)
-print("Database tables created.")
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
