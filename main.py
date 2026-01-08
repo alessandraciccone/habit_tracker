c24a45efb380729b624ae45a468660a5f7dadd02
alessandraciccone <alessandraciccone@icloud.com>
2026-01-08 10:02:14 +0100

    adds frontend origins

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.auth import router as auth_router
@@ -11,16 +12,18 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 # CORS
+origins = [
+    "http://localhost:5173",  # frontend Vite
+    "http://localhost:8000",  # opzionale se usi anche qui UI
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router, prefix="/auth")
 app.include_router(habits_router, prefix="/habits")
-
-from fastapi.middleware.cors import CORSMiddleware
-
